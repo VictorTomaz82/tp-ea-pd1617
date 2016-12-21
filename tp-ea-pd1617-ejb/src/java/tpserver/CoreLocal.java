816fdbd26d2ab78d35be02a95cc3754bbda4ac17
victor <victor@CyberClient>
2016-12-21 15:24:20 +0000

    add 21/12 (corelocal)

--- tp-ea-pd1617-ejb/src/java/tpserver/CoreLocal.java
+++ tp-ea-pd1617-ejb/src/java/tpserver/CoreLocal.java
@@ -1,10 +1,32 @@
 package tpserver;
 
+import java.util.ArrayList;
 import javax.ejb.Local;
+import logic.Category;
+import logic.Item;
+import logic.Report;
+import logic.User;
 
 @Local
 public interface CoreLocal {
 
+     ArrayList<User> getUsers();
+
+     void setUsers(ArrayList<User> users);
+
+     ArrayList<Report> getReports();
+
+     void setReports(ArrayList<Report> reports);
+
+     ArrayList<Item> getItems() ;
+
+     void setItems(ArrayList<Item> items);
+
+     ArrayList<Category> getCategories();
+
+     void setCategories(ArrayList<Category> categories);
+    
+    //
     String teste();
 
 }
