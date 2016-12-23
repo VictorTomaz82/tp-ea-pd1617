93075a53059515194c6ccf170bc459f952c770e8
korshar <andre_pinho_93@outlook.com>
2016-12-23 12:46:26 +0000

    Test

---
+++ tp-ea-pd1617-ejb/src/java/logic/Newsletter.java
@@ -0,0 +1,49 @@
+package logic;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Newsletter {
+
+    ArrayList<String> news;
+    Date date;
+    DateFormat dateFormat;
+
+    //--- Methods ---
+    //Constructor
+    public Newsletter() {
+        news = new ArrayList<>();
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        date = new Date();
+    }
+
+    public void setNews(ArrayList<String> news) {
+        this.news = news;
+    }
+    
+    public ArrayList<String> getNews() {
+        return news;
+    }
+
+    public void newAccount(String username) {
+        news.add(dateFormat.format(date) + " - New user registered : " + username);
+    }
+
+    public void userSuspendAccount(String username, String motive) {
+        news.add(dateFormat.format(date) + " - " + username + " has suspended his account for \"" + motive + "\"");
+    }
+
+    public void adminSuspendAccount(String username, String motive) {
+        news.add(dateFormat.format(date) + " - " + username + " has been suspended for \"" + motive + "\"");
+    }
+
+    public void accountReactivation(String username) {
+        news.add(dateFormat.format(date) + " - " + username + " asked for account reactivation");
+    }
+
+    public void itemSold(String name, int value) {
+        news.add(dateFormat.format(date) + " - The item " + name + " was sold for " + value + "€.");
+    }
+}
